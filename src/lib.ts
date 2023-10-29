67471e54009d0046d44298a108bc50b550d4a399
benjaminaaron <b.degenhart.work@gmail.com>
2023-10-29 19:27:43 +0100

    Remove two ts ignores (but gain a new one)

--- src/lib.ts
+++ src/lib.ts
@@ -1,14 +1,13 @@
-// @ts-ignore
 import SHACLValidator from "rdf-validate-shacl"
-// @ts-ignore
 import factory from "@zazuko/env-node"
 
 export async function runQueryOnProfile(queryName: string, profileName: string) {
     const shapes = await factory.dataset().import(factory.fromFile(`db/queries/${queryName}.ttl`))
     const data = await factory.dataset().import(factory.fromFile(`db/profiles/${profileName}.ttl`))
 
+    // @ts-ignore
     const validator = new SHACLValidator(shapes, { factory })
-    const report = await validator.validate(data)
+    const report = validator.validate(data)
 
     // get report details: https://github.com/zazuko/rdf-validate-shacl#usage
     // console.log(await report.dataset.serialize({ format: "text/n3" }))
