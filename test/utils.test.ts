2b8380c6ca1aba6187ff62d1f1515d920b9a4de7
wbglaeser <ben.glaeser@soundcloud.com>
2023-11-01 17:50:45 +0100

    set up testing facities

---
+++ test/utils.test.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import { expect } from 'chai';
+
+describe('Simple Test Group', function() {
+    it('should return true', function() {
+        expect(true).to.be.true;
+    });
+});
