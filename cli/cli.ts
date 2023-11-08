ccdf97f0e4032225c956695c9e59cd8bb096d658
wbglaeser <ben.glaeser@tuta.io>
2023-11-08 12:30:43 +0100

    add description to available commands

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,15 +1,58 @@
-import { printAllQueries, runQueryOnProfile, runAllQueriesOnProfile } from "../src/lib.js"
+import {printAllQueries, runQueryOnProfile, runAllQueriesOnProfile} from "../src/lib.js"
 
-const commands: { [key: string] : (...args: string[]) => void } = {
-    "print-all-queries": printAllQueries,
-    "run-query-on-profile": runQueryOnProfile,
-    "run-all-queries-on-profile": runAllQueriesOnProfile
+type commandType = {
+    description: string;
+    _function: (...args: string[]) => void;
+};
+
+const commands: { [key: string]: commandType } = {
+    "print-all-queries": {
+        description: "Print all available queries",
+        _function: printAllQueries
+    },
+    "run-query-on-profile": {
+        description: "Run single query on a profile: npm start run-query-on-profile <queryName> <profileName>",
+        _function: runQueryOnProfile
+    },
+    "run-all-queries-on-profile": {
+        description: "Run all queries on a profile: npm start run-all-queries-on-profile <profileName>",
+        _function: runAllQueriesOnProfile
+    }
 }
 
-const [, , command, ...args] = process.argv
+const maxLength = Math.max(...Object.keys(commands).map(command => command.length));
 
-if (commands[command]) {
-    commands[command](...args)
-} else {
-    console.error("Unknown command:", command)
+function displayHelp() {
+    console.log("Available Commands: \n");
+    for (let command in commands) {
+        console.log(`- ${command.padEnd(maxLength+6)}${commands[command].description}`)
+    }
 }
+
+export function processCommand(): void {
+
+    const [, , command, ...args] = process.argv
+
+    if (command === undefined) {
+        console.log("No command provided.");
+        displayHelp();
+        return;
+    }
+
+    if (!commands.hasOwnProperty(command)) {
+        console.log(`Invalid command: ${command}`);
+        displayHelp();
+        return;
+    }
+
+    console.log(`Processing command: ${command}`);
+
+    // Implement command-specific logic here
+    if (commands[command]) {
+        commands[command]._function(...args)
+    } else {
+        console.error("Unknown command:", command)
+    }
+}
+
+processCommand()
