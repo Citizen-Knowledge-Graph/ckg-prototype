f3fe1bf13dde5372f5078ff5dedb544d67e17153
wbglaeser <ben.glaeser@tuta.io>
2023-11-15 11:28:25 +0100

    remove some clutter

--- test/validator.test.ts
+++ test/validator.test.ts
@@ -7,8 +7,6 @@ import rdf from 'rdf-ext';
 jest.mock('../src/storage', () => ({
     getInstance: jest.fn().mockReturnValue({
         loadFile: jest.fn().mockImplementation((filepath: string) => {
-            // Return mocked RDF data based on the filepath
-            // This is where you simulate the file loading
         }),
     }),
 }));
@@ -16,7 +14,7 @@ jest.mock('../src/storage', () => ({
 describe('loadToShapes', () => {
     it('should load data from a file and return an RDF dataset', async () => {
         // Arrange
-        const filepath = './test/resources/empty.ttl'; // Replace with your test file path
+        const filepath = './test/resources/empty.ttl';
         const expectedDataset = rdf.dataset();
 
         // Act
@@ -26,11 +24,4 @@ describe('loadToShapes', () => {
         expect(result).toEqual(expectedDataset);
         // You can add more assertions here to validate specific aspects of the RDF dataset
     });
-
-    // it('should throw an error with an invalid file path', async () => {
-    //     const invalidFilePath = 'path/to/nonexistent/file';
-    //
-    //     // Expect a promise rejection to be thrown when calling the function with an invalid file path
-    //     await expect(loadToShapes(invalidFilePath)).rejects.toThrow();
-    // });
 });
